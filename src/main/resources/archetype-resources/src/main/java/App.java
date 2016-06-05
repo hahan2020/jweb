97824d18e460aa0f46e6d7815fa15fb6d7fdc724
liujinghui <345480567@qq.com>
2016-06-05 14:49:04 +0800

    init

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.hahan.jweb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
